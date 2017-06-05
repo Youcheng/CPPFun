c4272b7add4cbee1a2e83deb7b447049ae9a2070
Youcheng Lin <youcheng@charlesworthresearch.com>
2017-06-05 05:07:26 -0500

    delete

--- TypeTrait/staticAssert.cpp
+++ TypeTrait/staticAssert.cpp
@@ -7,6 +7,6 @@ int main() {
     int i = 5;
     int &j = i;
     j =6;
-    static_assert(std::is_reference<int>::value);
+    static_assert(std::is_reference<int &>::value);
 
 }
--- cmake-build-debug/CMakeFiles/throwExceptInDestructor.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for throwExceptInDestructor.
-# This may be replaced when dependencies are built.
