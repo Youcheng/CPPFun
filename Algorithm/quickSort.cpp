59633bed7fb3bb4561431e47c4d6466bf205ea3d
Youcheng Lin <youcheng@charlesworthresearch.com>
2017-07-14 04:14:17 -0500

    update

--- Algorithm/quickSort.cpp
+++ Algorithm/quickSort.cpp
@@ -1,4 +1,57 @@
-//
-// Created by ylin on 7/13/17.
-//
+#include <iostream>
+#include <utility>
 
+int findPivotIndex(int *a, int lIndex, int rIndex) {
+    int pIndex = lIndex;
+    bool useLIndex = false;
+    bool useRIndex = true;
+
+    while (lIndex < rIndex) {
+
+        if (useRIndex) {
+            if (a[rIndex] < a[pIndex]) {
+                std::swap(a[rIndex], a[pIndex]);
+                pIndex = rIndex;
+                useRIndex = false;
+                useLIndex = true;
+            } else {
+                rIndex--;
+            }
+        }
+
+        if (useLIndex) {
+            if (a[lIndex] > a[pIndex]) {
+                std::swap(a[lIndex], a[pIndex]);
+                pIndex = lIndex;
+                useRIndex = true;
+                useLIndex = false;
+            } else {
+                lIndex++;
+            }
+        }
+    }
+
+    return pIndex;
+}
+
+
+void quickSort(int *a, int start, int end) {
+    if(start < end) {
+        int pIndex = findPivotIndex(a, start, end);
+        quickSort(a, start, pIndex-1);
+        quickSort(a, pIndex+1, end);
+    }
+
+}
+
+int main() {
+
+    int arr[7] = {7, 2, 8, 1, 9, 1, 4};
+    quickSort(arr, 0, 6);
+
+    for (auto x: arr) {
+        std::cout << x << " " << std::endl;
+    }
+
+
+}
