12216e191ade48708fed9997c45be02f32c69cef
Youcheng Lin <youcheng@charlesworthresearch.com>
2017-07-19 02:28:40 -0500

    udpate

--- SmartPointer/cycliSharedPtr.cpp
+++ SmartPointer/cycliSharedPtr.cpp
@@ -12,39 +12,38 @@ struct A
 
 int main()
 {
-        std::shared_ptr<A> x = std::make_shared<A>();
+    std::shared_ptr<A> x = std::make_shared<A>();
 
-        std::cout << "&x " << &x << std::endl
-                  << "x.get() " << x.get() << std::endl
-                  << "&(x->ptr) " << &(x->ptr) << std::endl
-                  << "x->ptr.get() " << x->ptr.get() << std::endl
-                  << "x.use_count() " << x.use_count() << std::endl << std::endl;
+    std::cout << "&x " << &x << std::endl
+              << "x.get() " << x.get() << std::endl
+              << "&(x->ptr) " << &(x->ptr) << std::endl
+              << "x->ptr.get() " << x->ptr.get() << std::endl
+              << "x.use_count() " << x.use_count() << std::endl << std::endl;
 
-        x->ptr = x;
-        std::cout << "&x " << &x << std::endl
-                  << "x.get() " << x.get() << std::endl
-                  << "&(x->ptr) " << &(x->ptr) << std::endl
-                  << "x->ptr.get() " << x->ptr.get() << std::endl
-                  << "x.use_count() " << x.use_count() << std::endl << std::endl;
+    x->ptr = x;
+    std::cout << "&x " << &x << std::endl
+              << "x.get() " << x.get() << std::endl
+              << "&(x->ptr) " << &(x->ptr) << std::endl
+              << "x->ptr.get() " << x->ptr.get() << std::endl
+              << "x.use_count() " << x.use_count() << std::endl << std::endl;
 
-        // when x goes out of scope, the destructor of X will be called
-        // - decrement the reference to object of A by 1
-        // - check whether the current count is 0 but it is 1 since A.ptr still points to it.
-        // Therefore, A won't be destroyed.(memory leak)
+    // when x goes out of scope, the destructor of X will be called
+    // - decrement the reference to object of A by 1
+    // - check whether the current count is 0 but it is 1 since A.ptr still points to it.
+    // Therefore, A won't be destroyed.(memory leak)
 }
 
 // Example output
-//    /home/ylin/CLionProjects/CPPFun/cmake-build-debug/cycliSharedPtr
-//    A 0x14bbc30
-//      &x 0x7ffdb235d9f0
-//    x.get() 0x14bbc30
-//            &(x->ptr) 0x14bbc38
-//    x->ptr.get() 0
-//    x.use_count() 1
+//A 0x734c30
+//&x 0x7ffe47780180
+//x.get() 0x734c30
+//&(x->ptr) 0x734c38
+//x->ptr.get() 0
+//x.use_count() 1
 //
-//                  &x 0x7ffdb235d9f0
-//    x.get() 0x14bbc30
-//            &(x->ptr) 0x14bbc38
-//    x->ptr.get() 0x14bbc30
-//    x.use_count() 2
+//&x 0x7ffe47780180
+//x.get() 0x734c30
+//&(x->ptr) 0x734c38
+//x->ptr.get() 0x734c30
+//x.use_count() 2
 
--- SmartPointer/weak_ptr.cpp
+++ SmartPointer/weak_ptr.cpp
@@ -12,41 +12,41 @@ struct A
 
 int main()
 {
-        std::shared_ptr<A> x = std::make_shared<A>();
+    std::shared_ptr<A> x = std::make_shared<A>();
 
-        std::cout << "&x " << &x << std::endl
-                  << "x.get() " << x.get() << std::endl
-                  << "&(x->ptr) " << &(x->ptr) << std::endl
-                  << "x->ptr.get() " << x->ptr.lock().get() << std::endl
-                  << "x.use_count() " << x.use_count() << std::endl << std::endl;
+    std::cout << "&x " << &x << std::endl
+              << "x.get() " << x.get() << std::endl
+              << "&(x->ptr) " << &(x->ptr) << std::endl
+              << "x->ptr.get() " << x->ptr.lock().get() << std::endl
+              << "x.use_count() " << x.use_count() << std::endl << std::endl;
 
-        x->ptr = x;
-        std::cout << "&x " << &x << std::endl
-                  << "x.get() " << x.get() << std::endl
-                  << "&(x->ptr) " << &(x->ptr) << std::endl
-                  << "x->ptr.get() " << x->ptr.lock().get() << std::endl
-                  << "x.use_count() " << x.use_count() << std::endl << std::endl;
+    x->ptr = x;
+    std::cout << "&x " << &x << std::endl
+              << "x.get() " << x.get() << std::endl
+              << "&(x->ptr) " << &(x->ptr) << std::endl
+              << "x->ptr.lock().get() " << x->ptr.lock().get() << std::endl
+              << "x.use_count() " << x.use_count() << std::endl << std::endl;
 
-        // when x goes out of scope, the destructor of X will be called
-        // - decrement the reference to object of A by 1
-        // - check whether the current count is 0,
-        // - since we are using the weak_ptr, A.ptr points to the same object of A, so the current is actually 0 now.
-        // - then delete A* will be called to release that memory on the heap.
+    // when x goes out of scope, the destructor of X will be called
+    // - decrement the reference to object of A by 1
+    // - check whether the current count is 0,
+    // - since we are using the weak_ptr, A.ptr points to the same object of A, so the current is actually 0 now.
+    // - then delete A* will be called to release that memory on the heap.
 }
 
 
 // Example output
-//A 0x9a0c30
-//&x 0x7ffccb9a0430
-//x.get() 0x9a0c30
-//&(x->ptr) 0x9a0c38
+//A 0xf69c30
+//&x 0x7ffd5e0ca200
+//x.get() 0xf69c30
+//&(x->ptr) 0xf69c38
 //x->ptr.get() 0
 //x.use_count() 1
 //
-//&x 0x7ffccb9a0430
-//x.get() 0x9a0c30
-//&(x->ptr) 0x9a0c38
-//x->ptr.get() 0x9a0c30
+//&x 0x7ffd5e0ca200
+//x.get() 0xf69c30
+//&(x->ptr) 0xf69c38
+//x->ptr.lock().get() 0xf69c30
 //x.use_count() 1
 //
-//~A 0x9a0c30
+//~A 0xf69c30
